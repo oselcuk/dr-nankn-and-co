3fa4c12bf91b509c77e6aa31032fa1b24fabdb8e
Oguzhan Selcuk <oselcuk@outlook.com>
2017-04-24 22:46:54 -0400

    add profile pictures

--- app/src/main/java/com/DrNankn/cleanwater/Activities/EditProfileActivity.java
+++ app/src/main/java/com/DrNankn/cleanwater/Activities/EditProfileActivity.java
@@ -8,14 +8,24 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.DrNankn.cleanwater.Models.User;
 import com.DrNankn.cleanwater.R;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -25,6 +35,10 @@ public class EditProfileActivity extends AppCompatActivity {
     private int mShortAnimTime;
     private View mEditView;
     private View mProgressView;
+    private ImageButton mProfileImage;
+
+    private StorageReference mStorage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,11 +55,28 @@ public class EditProfileActivity extends AppCompatActivity {
         mNameView.setText(mActiveUser.getName());
 
         final Button mEditButton = (Button) findViewById(R.id.edit_button);
-        mEditButton.setOnClickListener(v -> editNewFields()
-        );
+        mEditButton.setOnClickListener(v -> editNewFields());
 
         final Button mCancelButton = (Button) findViewById(R.id.cancel_button);
         mCancelButton.setOnClickListener(v -> EditSuccessful(mActiveUser));
+
+        mProfileImage = (ImageButton) findViewById(R.id.profile_picture_button);
+        mStorage = FirebaseStorage.getInstance()
+                .getReference("profilePictures/" + mActiveUser.getEmail());
+
+        mStorage.getBytes(2<<20/*1mb*/)
+                .addOnSuccessListener(bytes -> {
+                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    mProfileImage.setImageBitmap(bmp);
+                }).addOnFailureListener(ex -> {
+            ex.toString();
+
+        });
+        mProfileImage.setOnClickListener(v -> {
+            Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(pickPhoto , 0);
+        });
     }
 
     /**
@@ -90,5 +121,40 @@ public class EditProfileActivity extends AppCompatActivity {
         result.putExtra("USER", user);
         setResult(Activity.RESULT_OK, result);
         finish();
+    }
+
+    private void changePicture(Bitmap bmp) {
+        int mx = Math.max(bmp.getWidth(), bmp.getHeight());
+        if (mx > 256) {
+            double scale = 256d / mx;
+            Bitmap old = bmp;
+            bmp = Bitmap.createScaledBitmap(bmp,
+                    (int)(bmp.getWidth() * scale), (int)(bmp.getHeight() * scale), true);
+            old.recycle();
+        }
+        mProfileImage.setImageBitmap(bmp);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] data = baos.toByteArray();
+        mStorage.putBytes(data);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        switch(requestCode) {
+            case 0:
+                try {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    InputStream stream = getContentResolver().openInputStream(selectedImage);
+                    Bitmap bmp = BitmapFactory.decodeStream(stream);
+                    changePicture(bmp);
+                } catch (FileNotFoundException _) {
+                    // not possible
+                }
+                break;
+        }
     }
 }
